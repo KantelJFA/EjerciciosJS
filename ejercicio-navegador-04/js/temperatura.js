bad10781d7cf62736ef8fdfcd783c4d4b9c8d506
KantelJFA <javifaa@gmail.com>
2026-05-28 01:56:55 +0200

    Preparada la estructura de archivos del ejercicio 4 de navegador.

---
+++ ejercicio-navegador-04/js/temperatura.js
@@ -0,0 +1,3 @@
+export const celsiusAFahrenheit = celsius => celsius * 9 / 5 + 32;
+// export const fahrenheitACelsius = fahrenheit => (fahrenheit - 32) * 5 / 9;
+// Soy consciente de que no es ideal usar funciones flecha para metodos.
